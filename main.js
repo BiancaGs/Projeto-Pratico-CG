592a078f59bb14eba8583056324a75a052d54b82
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-07-08 22:25:08 -0300

    Adição de uma SkyBox para maior realismo
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

--- main.js
+++ main.js
@@ -587,6 +587,34 @@ function init() {
     document.addEventListener('keydown', Teclado, false);
     window.addEventListener( 'resize', onWindowResize, false );
 
+
+    // =======================================================
+    // SKYBOX
+    // =======================================================
+
+    var materialArray = [];
+    let texture_ft = new THREE.TextureLoader(loadingManager).load('img/skybox/dust_ft.jpg');
+    let texture_bk = new THREE.TextureLoader(loadingManager).load('img/skybox/dust_bk.jpg');
+    let texture_up = new THREE.TextureLoader(loadingManager).load('img/skybox/dust_up.jpg');
+    let texture_dn = new THREE.TextureLoader(loadingManager).load('img/skybox/dust_dn.jpg');
+    let texture_rt = new THREE.TextureLoader(loadingManager).load('img/skybox/dust_rt.jpg');
+    let texture_lf = new THREE.TextureLoader(loadingManager).load('img/skybox/dust_lf.jpg');
+    
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_dn}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_rt}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_lf}));
+    
+    for (let i = 0; i < 6; i++)
+        materialArray[i].side = THREE.BackSide;
+
+    let skyboxGeo = new THREE.BoxGeometry(250, 250, 250);
+    let skybox = new THREE.Mesh(skyboxGeo, materialArray);
+
+    scene.add(skybox);
+
 }
 
 // Função animate (loop)
